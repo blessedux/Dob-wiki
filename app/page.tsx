42d4872ad84710b949e0231b1945b1d0f924f636
Joaquin <jfarfantorres@gmail.com>
2025-04-21 23:54:57 -0400

    Move background animation to page level for full-page effect

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,21 @@ import { SiteHeader } from "@/components/site-header"
 import { LandingHero } from "@/components/landing-hero"
 import { NavigationCards } from "@/components/navigation-cards"
 import { SiteFooter } from "@/components/site-footer"
+import { BackgroundPaths } from "@/components/ui/background-paths"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <SiteHeader />
-      <main className="flex-1">
-        <div className="max-w-screen-xl mx-auto px-8 md:px-10 lg:px-16">
-          <LandingHero />
-          <NavigationCards />
-        </div>
-      </main>
-      <SiteFooter />
-    </div>
+    <BackgroundPaths className="min-h-screen">
+      <div className="flex min-h-screen flex-col">
+        <SiteHeader />
+        <main className="flex-1">
+          <div className="max-w-screen-xl mx-auto px-8 md:px-10 lg:px-16">
+            <LandingHero />
+            <NavigationCards />
+          </div>
+        </main>
+        <SiteFooter />
+      </div>
+    </BackgroundPaths>
   )
 }
