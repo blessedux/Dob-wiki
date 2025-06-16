0c6371647022ed2544fe0012b5cd8108725a8826
Joaquin <jfarfantorres@gmail.com>
2025-06-15 22:41:34 -0400

    Add static build and update server configuration

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const next = require('next');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const app = express();
 
-app.prepare().then(() => {
-  const server = express();
+// Serve static files from the 'out' directory
+app.use(express.static(path.join(__dirname, 'out')));
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+// Handle all routes by serving index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'out', 'index.html'));
+});
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Static server running on http://localhost:${port}`);
 });
